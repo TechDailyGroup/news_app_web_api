add0deeb1f1c51ac11ba8c52ef74dc09c7c3715e
Simon <xrnie@outlook.com>
2019-08-29 15:52:02 +0800

    add some scripts

--- config.sh
+++ config.sh
@@ -30,62 +30,62 @@ PARAM_SECRET_KEY="p2^zm1jmbudhxi41lc6(t4d*%&a@rvp-7#igo-#^05+)27t=^@"
 
 function config_dev() {
 #  # SMTP
-#  sed -i -e "s|param_smtp_password|$PARAM_SMTP_PASSWORD|g" $FINAL_SETTINGS_FILE
+#  sed -i -e "s|param_smtp_password|${PARAM_SMTP_PASSWORD}|g" ${FINAL_SETTINGS_FILE}
 
   # ALLOWED HOST
+  # shellcheck disable=SC2068
   for host in ${PARAM_ALLOWED_HOST_DEBUG[@]};
   do
     HOST_STR1=${HOST_STR1}"\"${host}\", "
   done
-  sed -i -e "s|'param_debug_allowed_host'|$HOST_STR1|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|'param_debug_allowed_host'|${HOST_STR1}|g" ${FINAL_SETTINGS_FILE}
 
   # MYSQL
-  sed -i -e "s|param_db_name|$PARAM_MYSQL_DB_NAME|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_user|$PARAM_MYSQL_USER|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_password|$PARAM_MYSQL_PASSWORD|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_host|$PARAM_MYSQL_HOST|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_port|$PARAM_MYSQL_PORT|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|param_db_name|${PARAM_MYSQL_DB_NAME}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_user|${PARAM_MYSQL_USER}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_password|${PARAM_MYSQL_PASSWORD}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_host|${PARAM_MYSQL_HOST}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_port|${PARAM_MYSQL_PORT}|g" ${FINAL_SETTINGS_FILE}
 
   # DEPLOY_HOST
-#  sed -i -e "s|param_deploy_host|$PARAM_DEPLOY_HOST|g" $FINAL_SETTINGS_FILE
+#  sed -i -e "s|param_deploy_host|$PARAM_DEPLOY_HOST|g" ${FINAL_SETTINGS_FILE}
 
   # SECRET_KEY
-  sed -i -e "s|param_secret_key|$PARAM_SECRET_KEY|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|param_secret_key|${PARAM_SECRET_KEY}|g" ${FINAL_SETTINGS_FILE}
 }
 
 function config_prod() {
 #  # SMTP
-#  sed -i -e "s|param_smtp_password|$PARAM_SMTP_PASSWORD|g" $FINAL_SETTINGS_FILE
+#  sed -i -e "s|param_smtp_password|${PARAM_SMTP_PASSWORD}|g" ${FINAL_SETTINGS_FILE}
 
   # DEBUG
-  sed -i -e "s|DEBUG = True|DEBUG = False|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|DEBUG = True|DEBUG = False|g" ${FINAL_SETTINGS_FILE}
 
   # ALLOWED HOST
-  # shellcheck disable=SC2153
-  # shellcheck disable=SC1087
+  # shellcheck disable=SC2068
   for host in ${PARAM_ALLOWED_HOST_NOT_DEBUG[@]};
   do
     HOST_STR2=${HOST_STR2}"\"${host}\", "
   done
-  sed -i -e "s|'param_no_debug_allowed_host'|$HOST_STR2|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|'param_no_debug_allowed_host'|${HOST_STR2}|g" ${FINAL_SETTINGS_FILE}
 
   # MYSQL
-  sed -i -e "s|param_db_name|$PARAM_MYSQL_DB_NAME|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_user|$PARAM_MYSQL_USER|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_password|$PARAM_MYSQL_PASSWORD|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_host|$PARAM_MYSQL_HOST|g" $FINAL_SETTINGS_FILE
-  sed -i -e "s|param_db_port|$PARAM_MYSQL_PORT|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|param_db_name|${PARAM_MYSQL_DB_NAME}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_user|${PARAM_MYSQL_USER}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_password|${PARAM_MYSQL_PASSWORD}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_host|${PARAM_MYSQL_HOST}|g" ${FINAL_SETTINGS_FILE}
+  sed -i -e "s|param_db_port|${PARAM_MYSQL_PORT}|g" ${FINAL_SETTINGS_FILE}
 
 #  # DEPLOY_HOST
-#  sed -i -e "s|param_deploy_host|$PARAM_DEPLOY_HOST|g" $FINAL_SETTINGS_FILE
+#  sed -i -e "s|param_deploy_host|$PARAM_DEPLOY_HOST|g" ${FINAL_SETTINGS_FILE}
 
   # SECRET_KEY
-  sed -i -e "s|param_secret_key|$PARAM_SECRET_KEY|g" $FINAL_SETTINGS_FILE
+  sed -i -e "s|param_secret_key|${PARAM_SECRET_KEY}|g" ${FINAL_SETTINGS_FILE}
 
 }
 
 cd news_app || echo "cd news_app failed!"
-cp $ORIGIN_SETTINGS_FILE $FINAL_SETTINGS_FILE
+cp ${ORIGIN_SETTINGS_FILE} ${FINAL_SETTINGS_FILE}
 
 case $1 in
     dev)
--- main.sh
+++ main.sh
@@ -1,43 +1,44 @@
 #!/usr/bin/env bash
 
-# environment
-PARAM_ENVI="dev"
-
-# github params
-GITHUB_REPO="https://github.com/CodeinSimon/news_app_web_api.git"
-
-BRANCH_NAME="dev"
-
 # requirements path
-PARAM_REQUIREMENTS_PATH="./requirements/dev.txt"
+PARAM_REQUIREMENTS_PATH="./requirements.txt"
 
-# clone source code
-function git_clone() {
-  echo "Clone souce code from ${GITHUB_REPO} ${BRANCH_NAME}"
-  git clone -b ${BRANCH_NAME} ${GITHUB_REPO} || echo "Error occurs when cloning!"
-}
-
-# install requirements
 function install_requirements() {
-  cd news_app_web_api || echo "no such directory!"
-  echo "install requirements"
-  pip3 install -r ${PARAM_REQUIREMENTS_PATH} || echo "pip requirements install failed"
+  while read requirement; do pip install ${requirement}; done <${PARAM_REQUIREMENTS_PATH}
 }
 
 # config
 function config() {
-  echo "Config settings"
-  ./config.sh ${PARAM_ENVI} || echo "config failed!"
+  case ${1} in
+  "dev")
+    bash ./config.sh "dev" || echo "config failed!"
+    ;;
+  "prod")
+    bash ./config.sh "prod" || echo "config failed!"
+    ;;
+  *)
+    echo "Expect prod/dev!"
+    ;;
+
+  esac
 }
 # run server
 function start_service() {
-    echo "start service"
-    python3 manage.py runserver
+  echo "try to start service for ${1}"
+  python3 manage.py runserver || python manage.py runserver
 }
 
-git_clone
-install_requirements
-config
-start_service
-
-
+case $1 in
+install_requirements)
+  install_requirements
+  ;;
+config)
+  config "${2}"
+  ;;
+start_service)
+  start_service "${2}"
+  ;;
+*)
+  echo "usage: ./main.sh [install_requirements/config/start_service] [dev/prod]"
+  ;;
+esac
